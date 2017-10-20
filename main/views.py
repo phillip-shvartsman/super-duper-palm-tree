2f499ff9cd5a7a9ab4ee30c96471d1d80844c1fc
phillip-shvartsman <shvartsman.2@osu.edu>
2017-10-20 18:44:59 -0400

    Last commit before hackathon

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 from django.views.decorators.csrf import csrf_exempt
 from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+import json
 # Create your views here.
 
 def index(request):
@@ -12,6 +14,8 @@ def index(request):
 	return render(request,'main/index.html',context)
 @csrf_exempt
 def process(request):
-	print request.POST.get('id')
+	data = json.loads(request.POST['text']) #contains all the points .. access using data['0'] for first tuple .. data['0']['x'] for first
+		#tuples x value .. sadly they must be index using strings :(
+	print data 
 	#return render(request,'main/index.html',context)
 	return JsonResponse({'foo':'Hello!'});
--- static/js/editor.js
+++ static/js/editor.js
@@ -4,8 +4,65 @@ $(document).ready(function(){
 	var csrftoken = getCookie('csrftoken');
 	var currentPoint = [];
 	var pointHistory = [];
-	var checkIfClose = false;
-	var currentlineindex = 0;
+	var tempPoint = [];
+	var drawTemp = false;
+	var layerIndex = 0;
+	function drawTempFigures(mousePos)
+	{
+		x = mousePos.x;
+		y = mousePos.y;
+		$('#main-canvas').removeLayer('temp-line');
+		$('#main-canvas').removeLayer('temp-text');
+		if((currentPoint.x<x+10 && currentPoint.x>x-10))
+		{
+			//alert("!");
+			$('#main-canvas').addLayer({
+				type : 'line',
+				strokeStyle: '#000',
+				strokeWidth: 3,
+				name : 'temp-line',
+				fillStyle: '#c33',
+				x1: currentPoint.x, y1: currentPoint.y,
+				x2: currentPoint.x, y2: mousePos.y,
+			});
+			$('#main-canvas').addLayer({
+				type : 'text',
+				fillStyle: '#9cf',
+				strokeStyle: '#25a',
+				strokeWidth: 2,
+				name : 'temp-text',
+				x: currentPoint.x+10, y: mousePos.y,
+				fontSize: 12,
+				fontFamily: 'Calibri, sans-serif',
+				text: Math.abs(mousePos.y-currentPoint.y)/10 + 'meters'
+			});
+			tempPoint = {x:currentPoint.x,y:mousePos.y};
+		}
+		else if(currentPoint.y<y+10 && currentPoint.y>y-10)
+		{
+			$('#main-canvas').addLayer({
+				type : 'line',
+				strokeStyle: '#000',
+				strokeWidth: 3,
+				name : 'temp-line',
+				fillStyle: '#c33',
+				x1: currentPoint.x, y1: currentPoint.y,
+				x2: mousePos.x, y2: currentPoint.y,
+			});
+			$('#main-canvas').addLayer({
+				type : 'text',
+				fillStyle: '#9cf',
+				strokeStyle: '#25a',
+				strokeWidth: 2,
+				name : 'temp-text',
+				x: mousePos.x, y: (currentPoint.y - 10),
+				fontSize: 12,
+				fontFamily: 'Calibri, sans-serif',
+				text: Math.abs(mousePos.x-currentPoint.x)/10 + 'meters'
+			});
+			tempPoint = {x:mousePos.x,y:currentPoint.y};
+		}
+	}
 	function writeMessage(canvas, message,mousePos) {
 		$("#main-canvas").removeLayer('coordinates');
 		$('#main-canvas').addLayer({
@@ -19,47 +76,9 @@ $(document).ready(function(){
 			  fontFamily: 'Calibri, sans-serif',
 			  text: message
 		});
-		if(checkIfClose)
+		if(drawTemp)
 		{
-				x = mousePos.x;
-				y = mousePos.y;
-				$('#main-canvas').removeLayer('temp-line');
-				if((currentPoint.x<x+10 && currentPoint.x>x-10))
-				{
-					//alert("!");
-					$('#main-canvas').addLayer({
-					type : 'line',
-					strokeStyle: '#000',
-					strokeWidth: 2,
-					name : 'temp-line',
-					fillStyle: '#c33',
-					x1: currentPoint.x, y1: currentPoint.y,
-					x2: currentPoint.x, y2: mousePos.y,
-					});
-					$('#main-canvas').addLayer({
-						type : 'text',
-						fillStyle: '#9cf',
-						strokeStyle: '#25a',
-						strokeWidth: 2,
-						name : 'coordinates',
-						x: 80, y: 15,
-						fontSize: 12,
-						fontFamily: 'Calibri, sans-serif',
-						text: message
-					});
-				}
-				else if(currentPoint.y<y+10 && currentPoint.y>y-10)
-				{
-					$('#main-canvas').addLayer({
-					type : 'line',
-					strokeStyle: '#000',
-					strokeWidth: 2,
-					name : 'temp-line',
-					fillStyle: '#c33',
-					x1: currentPoint.x, y1: currentPoint.y,
-					x2: mousePos.x, y2: currentPoint.y,
-					});
-				}
+			drawTempFigures(mousePos);
 		}
 		$('#main-canvas').drawLayers();
 	}
@@ -93,38 +112,32 @@ $(document).ready(function(){
 			width: 5,
 			height: 5
 		}).drawLayers();
-		$('#main-canvas').off('click');
-		appendNewInstruction('Now use the given tools to draw a layout of your house');
-		currentPoint = mousePos;
-		checkIfClose = true;
-		pointHistory.push(mousePos);
 	}
 	function eventRectFormSubmit()
 	{
 		$("#rect-form").on("submit",function(event){
-		event.preventDefault();
-		data = $(this).serializeArray();
-		var length = data[0]['value'];
-		var width = data[1]['value'];
-		var unit = data[2]['value'];
-		if(unit=='meters')
-		{
-			length = length * meters;
-			width = width * meters;
-		}
-		if(unit=='feet')
-		{
-			length = length * feet;
-			width = width * feet;
-		}
-		$('#main-canvas').drawRect({
-				strokeStyle: '#c33',
-				strokeWidth: 1,
-				x: 100, y: 100,
-				width: length,
-				height: width
-		});
-		console.log($("#main-canvas")[0].getContext('2d'));
+			event.preventDefault();
+			data = $(this).serializeArray();
+			var length = data[0]['value'];
+			var width = data[1]['value'];
+			var unit = data[2]['value'];
+			if(unit=='meters')
+			{
+				length = length * meters;
+				width = width * meters;
+			}
+			if(unit=='feet')
+			{
+				length = length * feet;
+				width = width * feet;
+			}
+			$('#main-canvas').drawRect({
+					strokeStyle: '#c33',
+					strokeWidth: 1,
+					x: 100, y: 100,
+					width: length,
+					height: width
+			});
 		//return false;
 		});
 	}
@@ -132,7 +145,13 @@ $(document).ready(function(){
 	{
 		$('#main-canvas').on('click',function(e){
 			mousePos = getMousePos(this,e);
-			drawStartPoint(mousePos);	
+			drawStartPoint(mousePos);
+			$('#main-canvas').off('click');
+			appendNewInstruction('Now use the given tools to draw a layout of your house');
+			currentPoint = mousePos;
+			drawTemp = true;
+			pointHistory.push(mousePos);
+			eventNewVertex();	
 		});
 	}
 	function eventClearCanvas()
@@ -143,20 +162,22 @@ $(document).ready(function(){
 	}
 	function eventFirstUnitSelect()
 	{
-		
 		$("#unit-select").on('click',function(){
-			$("#main-container").fadeIn('slow',function(){
-				
-			});
+			var store = $("#unit-select").find('.active');
+			console.log(store);
+			$("#main-container").fadeIn('slow',function(){});
 		});
 	}
 	function eventSendToServer()
 	{
 		url = 'http://localhost:8000/process/';
-		data = {
-			id:6,
-			};
 		$("#send-data-to-server").on("click",function(){
+			var results = pointHistory.reduce(function(result, item, index, array) {
+			  result[index] = item; //a, b, c
+			  return result;
+			}, {})
+			results = JSON.stringify(results);
+			data = {text:results,unit:'meters'};
 			$.ajax({
 				url : url,
 				headers: {'X-CSRFToken':csrftoken},
@@ -176,6 +197,24 @@ $(document).ready(function(){
 	{
 		
 	}
+	function eventNewVertex()
+	{
+		$('#main-canvas').on('click',function(e){
+			console.log(pointHistory);
+			layerIndex += 1;
+			currentPoint = tempPoint;
+			pointHistory.push(tempPoint);
+			$('#main-canvas').setLayer('temp-line',{
+				name: 'line' + layerIndex,
+				strokeStyle: '#c33'
+			});
+			$('#main-canvas').setLayer('temp-text',{
+					name: 'text' + layerIndex,
+			});
+			
+			$('#main-canvas').drawLayers();
+		});
+	}
 	function eventRunAtStart()
 	{
 		eventCoordDisplay();
@@ -184,6 +223,7 @@ $(document).ready(function(){
 		eventFirstUnitSelect();
 		eventSendToServer();
 	}
+	
 	function getCookie(name) {
 		var cookieValue = null;
 		if (document.cookie && document.cookie != '') {
